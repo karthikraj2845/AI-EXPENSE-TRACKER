9a08db3a220f247c683f50ffd8cf16a8c188040a
PUDURI KARTHIK RAJ <karthikrajpuduri@gmail.com>
2026-07-02 23:00:52 +0530

    routes for summary,category-breakdown and monthly-trend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/transactions", transactionRoutes);
-
+app.use("/api/dashboard", dashboardRoutes);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
